a4a44bdb820cd288687bcbd77275d09969ae5fe1
Ilyass Arobaz <Ilyass.arobaz@gmail.com>
2015-09-24 02:12:58 +0200

    added ;

--- chat/config.js
+++ chat/config.js
@@ -24,14 +24,14 @@ params.charset                      = "utf-8";
 */
 
 /* Nick to be used. A % character will be replaced by a random number */
-params.nickAlternate                = "Arobaz%"
-params.ident                        = "Fuser"
+params.nickAlternate                = "Arobaz%";// j'enleve le ; ? t'in j'avais pas vu :o
+params.ident                        = "Fuser";
 params.rememberNickname             = true;
 params.showUserListInformationPopup = true;
 params.userListInformationPopupItems = "realname";
 params.performContinousWhoRequests   = true;
 showNickChangeButton = true;
-params.nick = "Arobaz%"
+params.nick = "Arobaz%"; //touche a rien att un truc va pas avec le % ok et regarde la 
 params.nickidentifypopup = true;
 params.identifyMessage              = "NickServ:Ce pseudo est enregistré";
 params.showJoinPartMessages        = false;
@@ -57,7 +57,7 @@ params.showNickSelection            = true;
 /* Adds a password field to the nick selection box */
 params.showIdentifySelection        = false;
 params.identifyMessage              = "NickServ:Ce pseudo est enregistré";
-params.identifyCommand              = "/msg NickServ identify %pass%"
+params.identifyCommand              = "/msg NickServ identify %pass%";
 params.userListCustomFormatting = "realname=(^|\\W)([0-9]+\\s)?[mMHh](\\s|$):userListMan,realname=(^|\\W)([0-9]+\\s)?[fFWw](\\s|$):userListWoman,mode=v:channelListVoice,mode=h:channelListModo,mode=o:channelListOp,mode=a:channelListAdmin,mode=q:channelListOwner";
 
 /* Show button to register a nickname */
